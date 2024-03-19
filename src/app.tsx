436800c1bf10d4e9bf672798ea75ba6fb1ae302d
25huizengek1 <50515369+25huizengek1@users.noreply.github.com>
2024-03-19 21:41:19 +0100

    Migrate to Solid Start v0.7.7

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,10 @@
 import { Router } from "@solidjs/router";
-import { FileRoutes } from "@solidjs/start";
+import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
 
 export default function App() {
-    return (
-        <Router
-            root={props => (
-                <>
-                    <Suspense>{props.children}</Suspense>
-                </>
-            )}
-        >
-            <FileRoutes />
-        </Router>
-    );
+    return <Router root={props => <Suspense>{props.children}</Suspense>}>
+        <FileRoutes />
+    </Router>;
 }
