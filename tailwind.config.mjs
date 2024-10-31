1b1007317b30662e1ee5502990e7e65b7c9784a4
0039dope <arnavkaushal09@gmail.com>
2024-10-31 09:41:34 +0000

    github btn

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,12 +5,26 @@ export default {
     extend: {
       animation: {
         jiggle: "jiggle 0.5s ease-in-out both infinite",
+        tada: "tada 3s ease-in-out both infinite",
       },
       keyframes: {
         jiggle: {
           "0%": { transform: "rotate(-3deg)" },
           "50%": { transform: "rotate(3deg)" },
           "100%": { transform: "rotate(-3deg)" },
+        },
+        tada: {
+          "0%": { transform: "scale(1)" },
+          "10%": { transform: "scale(0.9) rotate(-3deg)" },
+          "20%": { transform: "scale(0.9) rotate(-3deg)" },
+          "30%": { transform: "scale(1.1) rotate(3deg)" },
+          "40%": { transform: "scale(1.1) rotate(-3deg)" },
+          "50%": { transform: "scale(1.1) rotate(3deg)" },
+          "60%": { transform: "scale(1.1) rotate(-3deg)" },
+          "70%": { transform: "scale(1.1) rotate(3deg)" },
+          "80%": { transform: "scale(1.1) rotate(-3deg)" },
+          "90%": { transform: "scale(1.1) rotate(3deg)" },
+          "100%": { transform: "scale(1) rotate(0)" },
         },
       },
     },
